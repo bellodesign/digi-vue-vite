1ff77342653eb5d0a784fb17751882b6679ff220
Marko Maberg <marko.keranen-maberg@arbetsformedlingen.se>
2024-03-28 13:22:19 +0100

    Added example code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
-import { ButtonVariation } from '@digi/core';
+import { CodeBlockLanguage } from '@digi/core';
+
+const codeBlock = `
+plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => tag.startsWith('digi-')
+      }
+    }
+  })]
+`;
 </script>
 
 <template>
   <digi-typography>
     <digi-layout-block>
       <h1>Digi - vue/vite</h1>
-      <digi-button :af-variation="ButtonVariation.FUNCTION">
-        <digi-icon-copy slot="icon" />Kopiera</digi-button>
+      <p>A lab that uses components created by The Swedish Public Employment Service</p>
+    </digi-layout-block>
+    <digi-layout-block>
+      <digi-expandable-accordion af-heading="Lab information">
+        <p>This is a Vue environment created with Vite</p>
+        <p>To avoid warnings in console this was added to <digi-code af-code="vite.config.ts" />:</p>
+        <p>
+          <digi-code-block :af-code="codeBlock" :af-language="CodeBlockLanguage.JAVASCRIPT" />
+        </p>
+      </digi-expandable-accordion>
     </digi-layout-block>
   </digi-typography>
 </template>
