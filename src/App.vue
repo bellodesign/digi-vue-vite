fcabc019392075781f1c6a4c2965e6117c3d0e7d
Marko Maberg <marko.keranen-maberg@arbetsformedlingen.se>
2024-03-28 13:09:19 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ButtonVariation } from '@digi/core';
+</script>
+
+<template>
+  <digi-typography>
+    <digi-layout-block>
+      <h1>Digi - vue/vite</h1>
+      <digi-button :af-variation="ButtonVariation.FUNCTION">
+        <digi-icon-copy slot="icon" />Kopiera</digi-button>
+    </digi-layout-block>
+  </digi-typography>
+</template>
